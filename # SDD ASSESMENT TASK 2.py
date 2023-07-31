387c30c29e680cdbad3d9c40fc1daaae3834c449
OK <Ojas.kumar@education.nsw.gov.au>
2023-07-31 14:11:29 +1000

    Updated Code
    Code was updated - fundamentals were added

--- # SDD ASSESMENT TASK 2.py
+++ # SDD ASSESMENT TASK 2.py
@@ -1,1 +1,13 @@
 # SDD ASSESMENT TASK 2
+
+import tkinter
+import tkinter.messagebox
+import customtkinter as ctk
+import googletrans
+
+#Setting customTkinter theme
+customtkinter.set_appearance_mode("System")  
+customtkinter.set_default_color_theme("blue")
+
+#Creating a window
+root = ctk.CTk
